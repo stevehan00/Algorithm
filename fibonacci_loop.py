90d60543ca1e88685fe3136fab4d8d7c8a45ad8c
Seungoh Han <s5zh@o.cnu.ac.kr>
2019-12-31 18:17:12 +0900

    Create fibonacci_loop.py

---
+++ fibonacci_loop.py
@@ -0,0 +1,14 @@
+#O(n)
+def fib(n: int) -> int:
+    if n == 0:
+        return n
+    last: int = 0
+    next: int = 1   
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
+
+
+if __name__ == "__main__":
+    s = int(input())
+    print(fib(s))
